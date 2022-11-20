a0626b867488941deb4831f0053216819df7d19d
Yago <yago.lm@samsung.com>
2022-11-20 00:17:11 -0300

    Banco de dados e model

---
+++ database/database.js
@@ -0,0 +1,10 @@
+const Sequelize = require("sequelize")
+
+// Option 3: Passing parameters separately (other dialects)
+const connection = new Sequelize('perguntas', 'root', 'root', {
+    host: 'localhost',
+    dialect: 'mysql'/* one of  | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+});
+
+module.exports = connection
+
--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 const express = require("express")
+const bodyParser = require("body-parser")
+const connection = require("./database/database")
+const PerguntaModelDB = require("./database/Pergunta")
+
+// try {
+//     await connection.authenticate();
+//     console.log('Connection has been established successfully.');
+// } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+// }
+
+connection.authenticate()
+    .then(() =>{
+        console.log('Connection has been established successfully.');
+    })
+    .catch((error) => {
+        console.error('Unable to connect to the database:', error);
+    })
 
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+
 app.get("/", (req, res) =>{
-
     var name = "Yago"
-
     // res.send("Bem vindo")
     res.render("index", { name:name, language: "JS" })
 })
 
+app.post("/salvarpergunta", (req, res) =>{
+    var titulo  = req.body.titulo
+    var descricao  = req.body.descricao
+
+    console.log(titulo + " - " + descricao)
+})
 
 app.listen(8080, ()=> {console.log("App executando...")})
